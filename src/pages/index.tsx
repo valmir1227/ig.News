1dde253e8543e747fa1c1d91e93fd585a064f2a1
Valmir Almeida <almeidavalmir76@gmail.com>
2022-04-30 21:08:30 -0300

    Titulos dinamicos por páginas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,13 @@
 import styles from "../styles/home.module.scss";
+import Head from "next/head";
 
 export default function Home() {
-  return <h1 className={styles.title}>Hello Next.js</h1>;
+  return (
+    <>
+      <Head>
+        <title>Home | igNews</title>
+      </Head>
+      <h1 className={styles.title}>Hello Next.js</h1>
+    </>
+  );
 }
