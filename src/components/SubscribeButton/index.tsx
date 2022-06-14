af1f350622de3ff399248c6e1ae23d992625923b
Valmir Almeida <almeidavalmir76@gmail.com>
2022-06-14 20:17:58 -0300

    start create stripeCustomer

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,7 +1,28 @@
+import { signIn, useSession } from "next-auth/react";
 import styles from "./styles.module.scss";
-export function SubscribeButton() {
+
+interface SubscribeButtonProps {
+  priceId: string;
+}
+
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { session } = useSession();
+
+  function handleSubscribe() {
+    // Se o usuário não estiver logado, chama a função de login
+    if (!session) {
+      signIn("github");
+      return;
+    }
+    //Se tiver logado cria a checkout session []
+  }
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe Now
     </button>
   );
